0ef1c36682ddf230ecf5c830284487f58d42f431
Tramble 51 <tramble515@gmail.com>
2024-12-06 04:10:13 +0000

    Update m.py

--- m.py
+++ m.py
@@ -578,7 +578,7 @@ def show_stop_action_button(message):
 
 def run_action(user_id, message, ip, port, duration):
     try:
-        numbers = [60, 90]
+        numbers = [200, 300]
         thread_value = random.choice(numbers)
         # Notify the user that the action started
         bot.reply_to(message, f"🎉 *Socket Connected in {thread_value}ms*", parse_mode='Markdown')
