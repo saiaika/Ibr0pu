88a157c3778885eab7ba1812978e833d1af83fb0
Tramble 51 <tramble515@gmail.com>
2025-03-07 20:31:18 +0000

    Edit m.py

--- m.py
+++ m.py
@@ -28,7 +28,7 @@ logging.basicConfig(filename='bot_actions.log', level=logging.INFO,
                     format='%(asctime)s - %(message)s')
 
 # Initialize the bot with the token from environment variables
-TOKEN = "7267969157:AAFBW9fqZYa1kMnAB9CerIxWQnJ0-6c7Wns"
+TOKEN = "7589844293:AAG2nNWmwjbtl9bP2RkYAFJreftSJV6-n4o"
 if not TOKEN:
     raise ValueError("Please set your bot token in the environment variables!")
 
